1df1bf85239a8b731a94afcb225c934de7b33241
david-frederick <warder57@gmail.com>
2020-02-26 01:39:38 -0500

    add some basic game scopes

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,16 @@
 class Game < ApplicationRecord
+  scope :owned,        -> { where(own: true) }
+  scope :wishlist,     -> { where(wishlist: true) }
+  scope :preordered,   -> { where(preordered: true) }
+  scope :for_trade,    -> { where(for_trade: true) }
+  scope :prevowned,    -> { where(prevowned: true) }
+  scope :must_have,    -> { where(wishlist: true, wishlist_priority: 1) }
+  scope :love_to_have, -> { where(wishlist: true, wishlist_priority: 2) }
+  scope :like_to_have, -> { where(wishlist: true, wishlist_priority: 3) }
+  scope :considering,  -> { where(wishlist: true, wishlist_priority: 4) }
+  scope :dont_want,    -> { where(wishlist: true, wishlist_priority: 5) }
+  scope :want_to_play, -> { where(wanttoplay: true) }
+
   def self.upsert_from_api_obj!(api_obj)
     game = self.find_or_initialize_by(bgg_id: api_obj['@objectid'])
     game.update(

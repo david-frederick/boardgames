0bd15a69be5764a1f2a4beabfc427719d68a4702
david-frederick <warder57@gmail.com>
2020-02-26 00:54:15 -0500

    games#index link to basic games#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'game/index'
+  resources :game, only: [:index, :show]
 
   root to: 'game#index'
 end

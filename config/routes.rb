9f578c2df75a87392e12d0b29bea11c200588817
david-frederick <warder57@gmail.com>
2020-02-27 18:06:19 -0500

    basic navbar + collection#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'collection/index'
   resources :game, only: [:index, :show]
-
-  root to: 'game#index'
+  resources :collection, only: [:index]
+  root to: 'collection#index'
 end
